c0fa5e9c854e9d1e225c9db58dd4defa59e5c1b3
anjali234 <30770861+anjali234@users.noreply.github.com>
2017-08-29 01:02:27 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,200 +1,97 @@
 package com.example.user.environment;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.graphics.Color;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.app.Activity;
 import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.Viewport;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-
-public class MainActivity extends Activity implements SensorEventListener {
-    private Sensor mPressure;
-
-    private SensorManager manager;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private TextView intensity;
+    private TextView pressure;
+    private TextView temperature;
+    private TextView humidity;
+    float x,y,z,p,t;
+    float normalizedValue;
     private Sensor accelerometer;
-    private float x;
-    private float y;
-    private float z;
-    private float normalizedValue;
-    long timestamp;
-    private LineGraphSeries<DataPoint> mSeries;
-    private LineGraphSeries<DataPoint> mSeries2;
-    private LineGraphSeries<DataPoint> mSeries3;
-    private LineGraphSeries<DataPoint> mSeries4;
-    //shows unfiltered accelerometer data
-    private GraphView graph;
-    //shows filtered accelerometer data
-    private GraphView graph2;
-    private GraphView graph3;
-    private GraphView graph4;
-    private double lastXValue = 5d;
-    //Variable to store appended data points
-    float data[] = new float[500];
-    double timestamps[] = new double[500];
-    double coefficients[];
-    static int index = 0;
+    float array[];
+    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        graph = (GraphView) findViewById(R.id.graph);
-        graph2 = (GraphView) findViewById(R.id.graph2);
-        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        //Setting GraphView Viewport layouts
-        Viewport gViewport =  graph.getViewport();
-        Viewport gViewport2 = graph2.getViewport();
-        gViewport.setXAxisBoundsManual(true);
-        gViewport.setMinX(0);
-        gViewport.setScrollable(true);
-        gViewport.setMinY(0);
-        gViewport.setScalable(true);
-        // activate horizontal and vertical zooming and scrolling
-        gViewport.setScalableY(true);
-        // activate vertical scrolling
-        gViewport.setScrollableY(true);
-        gViewport2.setXAxisBoundsManual(true);
-        gViewport2.setMinX(0);
-        gViewport2.setScrollable(true);
-        gViewport2.setMinY(0);
-        gViewport2.setScalable(true);
-        // activate horizontal and vertical zooming and scrolling
-        gViewport2.setScalableY(true);
-        // activate vertical scrolling
-        gViewport2.setScrollableY(true);
-        //add blank mseries to graph
-        mSeries = new LineGraphSeries<>();
-        //mseries features
-        mSeries.setTitle("Acceleration curve");
-        mSeries.setColor(Color.GREEN);
-        mSeries.setDrawDataPoints(true);
-        mSeries.setDataPointsRadius(10);
-        mSeries.setThickness(8);
-        graph.addSeries(mSeries);
-        mSeries2 = new LineGraphSeries<>();
-        //mseries features
-        mSeries2.setTitle("Filtered curve");
-        mSeries2.setColor(Color.BLUE);
-        mSeries2.setDrawDataPoints(true);
-        mSeries2.setDataPointsRadius(7);
-        mSeries2.setThickness(6);
-        graph2.addSeries(mSeries2);
-
-        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mPressure = manager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        mSensorManager = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        array=new float[500];
     }
 
-    private void gViewport() {
-
-    }
-
+    /*Method called on sensor event change*/
     @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Do something here if sensor accuracy changes.
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_PRESSURE){
-            x=sensorEvent.values[0];
-            y=sensorEvent.values[1];
-            z=sensorEvent.values[2];
+    public void onSensorChanged(SensorEvent event) {
+        TextView tvX = (TextView)findViewById(R.id.lightIntensity);
+        TextView tvY = (TextView)findViewById(R.id.pressure);
+        TextView tvZ = (TextView)findViewById(R.id.humidity);
+        temperature = (TextView)findViewById(R.id.temperature);
+        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER) {
+            //acceleration retrieved from the event and the gravity is removed
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
             normalizedValue = (float) Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
-            timestamp=System.currentTimeMillis();
-            lastXValue += 1d;
-            //Append data to plot graph whenever there is a change i acceleration plots upto 500 data changes
-            mSeries.appendData(new DataPoint(lastXValue,normalizedValue), true, 500);
-            while(index < 500) {
-                data[index] = normalizedValue;
-                timestamps[index] = lastXValue;
-                index++;
-                break;
+            temperature.setText("Acceleration:"+Float.toString(normalizedValue));
+            while(i<499){
+                array[i]=normalizedValue;
+                i++;
             }
+        }
+        else if(event.sensor.getType()==Sensor.TYPE_LIGHT){
+            tvX.setText("Light:"+Float.toString(event.values[0]));
+        }
+        else if(event.sensor.getType()==Sensor.TYPE_PROXIMITY){
+            tvZ.setText("Proximity:"+Float.toString(event.values[0]));
+        }
+        else if(event.sensor.getType()==Sensor.TYPE_PRESSURE){
+            tvY.setText("Pressure: "+Float.toString(event.values[0]));
         }
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
-            x=sensorEvent.values[0];
-            y=sensorEvent.values[1];
-            z=sensorEvent.values[2];
-            normalizedValue = (float) Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
-            timestamp=System.currentTimeMillis();
-            lastXValue += 1d;
-            //Append data to plot graph whenever there is a change i acceleration plots upto 500 data changes
-            mSeries.appendData(new DataPoint(lastXValue,normalizedValue), true, 500);
-            while(index < 500) {
-                data[index] = normalizedValue;
-                timestamps[index] = lastXValue;
-                index++;
-                break;
-            }
-        }
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_LIGHT){
-            x=sensorEvent.values[0];
-            y=sensorEvent.values[1];
-            z=sensorEvent.values[2];
-            normalizedValue = (float) Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
-            timestamp=System.currentTimeMillis();
-            lastXValue += 1d;
-            //Append data to plot graph whenever there is a change i acceleration plots upto 500 data changes
-            mSeries.appendData(new DataPoint(lastXValue,normalizedValue), true, 500);
-            while(index < 500) {
-                data[index] = normalizedValue;
-                timestamps[index] = lastXValue;
-                index++;
-                break;
-            }
-        }
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_AMBIENT_TEMPERATURE){
-            x=sensorEvent.values[0];
-            y=sensorEvent.values[1];
-            z=sensorEvent.values[2];
-            normalizedValue = (float) Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
-            timestamp=System.currentTimeMillis();
-            lastXValue += 1d;
-            //Append data to plot graph whenever there is a change i acceleration plots upto 500 data changes
-            mSeries.appendData(new DataPoint(lastXValue,normalizedValue), true, 500);
-            while(index < 500) {
-                data[index] = normalizedValue;
-                timestamps[index] = lastXValue;
-                index++;
-                break;
-            }
-        }
-    }
-    
-    @Override
-    protected void onResume() {
-        // Register a listener for the sensor.
+    public void onResume(){
         super.onResume();
-        manager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener((SensorEventListener) this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener((SensorEventListener) this,mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener((SensorEventListener) this,mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY),SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener((SensorEventListener) this,mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE),SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
-    protected void onPause() {
-        // Be sure to unregister the sensor when the activity pauses.
+    public void onPause(){
         super.onPause();
-        manager.unregisterListener(this);
+        mSensorManager.unregisterListener((SensorEventListener)this);
+    }
+
+    public void onPlotClick(View v){
+        //Starting a new Intent
+        Intent nextScreen = new Intent(getApplicationContext(), PlotData.class);
+
+        //Sending data to another Activity
+        nextScreen.putExtra("myarray", array);
+
+        Log.e("n", temperature.getText().toString());
+
+        startActivity(nextScreen);
     }
 }
 
